364f01822592f5b25abce5406c522cb7919675bc
Luis Gutierrez <35284988+GaloisAim@users.noreply.github.com>
2019-11-07 16:28:56 -0300

    Create Gig Table

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        
+        
+        public int Id { get; set; }
+       
+        //Required make this attributes or propperties not null AKA cant be null/ not nullable
+        [Required]
+        public ApplicationUser Artist { get; set; }
+       
+        public DateTime DateTime { get; set; }
+
+
+        [Required]
+        //I want lo limit this fields to 250 characters so..
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        //Each required attribute means a line break
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
