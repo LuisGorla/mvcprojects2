68c9dc739d1aed1758d2cc252c6b55cd28e3c383
Luis Gutierrez <35284988+GaloisAim@users.noreply.github.com>
2019-11-12 11:03:08 -0300

    Add Gig Form

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Models;
+using GigHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        // GET: Gigs
+        private readonly ApplicationDbContext _context;
+        
+        public GigsController()
+        {
+            _context = new ApplicationDbContext(); // What about dependency injection and the repository pattern?
+        }
+        public ActionResult Create()
+        {
+            //To list the genres in the listbox we need to populate the Genres property in the View Model, and this is the job of the controller 
+            //We need the list of genres from the database, for this we need the DbContext
+
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            //This view model inside the view
+
+            return View(viewModel);
+        }
+
+        
+    }
+}
---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GigHub.Models;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        //string because everithing that the user puts in the form is a string. Later we will be parsing this string into objects of different types
+
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+
+        //with dropdown list we need numeric value for each option
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
