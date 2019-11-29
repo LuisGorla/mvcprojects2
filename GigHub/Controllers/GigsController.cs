c98579563c830dba87d6e637d437cf096f7e5b08
Luis Gutierrez <35284988+GaloisAim@users.noreply.github.com>
2019-11-29 09:36:54 -0300

    Anti Forgery Implementation

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,14 +1,14 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class GigsController : Controller
-    {
+    {   
+        // A controller should act as a coordinator of the app logic / What should happen next? / Should work as a manager
         // GET: Gigs
         private readonly ApplicationDbContext _context;
         
@@ -35,15 +35,21 @@ namespace GigHub.Controllers
 
         [Authorize]
         [HttpPost]// we want this attribute to only be called by an httppost method
+        [ValidateAntiForgeryToken]// antiforgery token 
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
             //convert viewModel to GigObject, add it to context and save changes
 
             var gig = new Gig // So each gig has an artist, a DateTime, a Genre and a Venue (place), this is a Gig object
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),//DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
-                //associate gig with genre object, similar to artist, we need to read it from the database first
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
 
--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -1,20 +1,39 @@
 ﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigFormViewModel
     {
         //string because everithing that the user puts in the form is a string. Later we will be parsing this string into objects of different types
-
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
 
 
-        //with dropdown list we need numeric value for each option
-        public byte Genre { get; set; }
-        public IEnumerable<Genre> Genres { get; set; }
+        [Required]
+        public byte Genre { get; set; }//with dropdown list we need numeric value for each option
 
+        public IEnumerable<Genre> Genres { get; set; }
+        public DateTime GetDateTime()
+        { 
+
+            
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+                //Because we are inside the viewModel, i dont need viewmodel.something
+                //DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                //associate gig with genre object, similar to artist, we need to read it from the database first
+                //Here we are making the controller responsable of parsint a string into datetime object. The class who knows about dates and time should be the one parsing, in this case viewModel
+            
+        }
     }
 }
